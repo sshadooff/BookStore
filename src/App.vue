a2f5c4479333316adabe16b6971d5505c6d3db47
Tatyana <juravleva.tanya01@yandex.ru>
2025-10-06 10:20:21 +0300

    Сделана главная страница

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,41 @@
 <template>
-  <div>
-    Работает!
+  <div class="wrap">
+    <Header></Header>
+    <main class="content">
+      <router-view></router-view>
+    </main>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
+import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
 
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+}
 </script>
 
 <style>
-  
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.wrap {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;;
+  color: #333;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;  
+}
+
+.content {
+  flex: 1;
+}
 </style>
